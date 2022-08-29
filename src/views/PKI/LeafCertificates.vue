5f17c0b9e3e0bd810096f8aa6340d78eb8e492bc
Jain, Ankur <Ankur_Jain@comcast.com>
2022-08-29 09:32:52 -0400

    Add PKI Download and Validator Transactions

--- src/views/PKI/LeafCertificates.vue
+++ src/views/PKI/LeafCertificates.vue
@@ -1,135 +1,201 @@
 <script>
-import DataTable from 'primevue/datatable'
-import Column from 'primevue/column'
-import Button from 'primevue/button'
-import Dialog from 'primevue/dialog'
-import Message from 'primevue/message'
-import GrantActionRootCertificate from './GrantActionRootCertificate'
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Button from "primevue/button";
+import Dialog from "primevue/dialog";
+import Message from "primevue/message";
+import GrantActionRootCertificate from "./GrantActionRootCertificate";
 
 export default {
-  name: 'LeafCertificates',
-  props: ['subject', 'subjectKeyId'],
+	name: "LeafCertificates",
+	props: ["subject", "subjectKeyId"],
 
-  data() {
-    return {
+	data() {
+		return {
 			showRevokeCert: false,
 			revokeCertificate: null,
 			expandedRows: [],
-    }
-  },
+		};
+	},
 
-  methods: {
+	methods: {
 		showRevokeCertificateDialog(certificate) {
-			this.showRevokeCert = true
-			this.revokeCertificate = certificate
+			this.showRevokeCert = true;
+			this.revokeCertificate = certificate;
 		},
 
 		dismissRevokeCertificateDialog() {
-			this.showRevokeCert = false
-		}
+			this.showRevokeCert = false;
+		},
 
-  },
-  components: {
-    DataTable,
-    Column,
-    Button,
-    Dialog,
-    Message,
+		// Download the certificate in PEM format
+		downloadCertificateChain(certificate) {
+			let filename = "certificate-chain.pem";
+			let element = document.createElement("a");
+			certificate = this.getCertificate(certificate.subjectKeyId);
+			let certificateChain = this.getCertificateChain(certificate);
+			let pemCert = "";
+			certificateChain.forEach((certificate) => {
+				pemCert =
+					pemCert +
+					"Subject: " +
+					certificate.certs[0].subjectAsText +
+					"\n" +
+					certificate.certs[0].pemCert +
+					"\n\n";
+			});
+			element.setAttribute(
+				"href",
+				"data:application/json;charset=utf-8," + encodeURIComponent(pemCert)
+			);
+			element.setAttribute("download", filename);
+			element.style.display = "none";
+			document.body.appendChild(element);
+			element.click();
+			document.body.removeChild(element);
+		},
+
+		getCertificateChain(certificate) {
+			let chain = [];
+			while (certificate != null) {
+				chain.push(certificate);
+				certificate = this.getCertificate(certificate.certs[0].authorityKeyId);
+			}
+			return chain;
+		},
+
+		getCertificate(subjectKeyId) {
+			return this.allApprovedCertificates.find((cert) => {
+				return cert.subjectKeyId === subjectKeyId;
+			});
+		},
+	},
+	components: {
+		DataTable,
+		Column,
+		Button,
+		Dialog,
+		Message,
 		GrantActionRootCertificate,
-  },
+	},
 
-  computed: {
+	computed: {
+		allApprovedCertificates() {
+			const approvedCertificatesArray =
+				this.$store.getters[
+					"zigbeealliance.distributedcomplianceledger.pki/getApprovedCertificatesAll"
+				]();
+			return approvedCertificatesArray.approvedCertificates;
+		},
 
+		allChildCertificates() {
+			let nonRootCertificates = [];
+			const approvedCertificatesArray =
+				this.$store.getters[
+					"zigbeealliance.distributedcomplianceledger.pki/getApprovedCertificatesAll"
+				]();
+			if (approvedCertificatesArray.approvedCertificates) {
+				let allCerts = approvedCertificatesArray.approvedCertificates;
+				nonRootCertificates = allCerts.filter((cert) => {
+					return !cert.certs[0].isRoot;
+				});
+				nonRootCertificates = nonRootCertificates.map((item) => {
+					item.approvals = item.certs[0].approvals;
+					item.serialNumber = item.certs[0].serialNumber;
+					item.subjectAsText = item.certs[0].subjectAsText;
+					return item;
+				});
+			}
 
-		allCertificates() {
 			let childCerts = [];
-			const certificatesArray = this.$store.getters['zigbeealliance.distributedcomplianceledger.pki/getChildCertificates']({
-            params: {
-							issuer: this.subject,
-							authorityKeyId: this.subjectKeyId
-            }
-          });
+			const certificatesArray = this.$store.getters[
+				"zigbeealliance.distributedcomplianceledger.pki/getChildCertificates"
+			]({
+				params: {
+					issuer: this.subject,
+					authorityKeyId: this.subjectKeyId,
+				},
+			});
 			if (certificatesArray && certificatesArray.childCertificates) {
 				childCerts = certificatesArray.childCertificates.certIds;
+
+				childCerts = childCerts.map((childCert) => {
+					let cert = nonRootCertificates.find(
+						(item) => item.subjectKeyId == childCert.subjectKeyId
+					);
+					childCert.subjectAsText = cert.certs[0].subjectAsText;
+					return childCert;
+				});
 			}
+
 			return childCerts;
 		},
 
-		    isSignedIn() {
-      const loggedIn = this.$store.getters['common/wallet/loggedIn']
-      return loggedIn
-    }
+		isSignedIn() {
+			const loggedIn = this.$store.getters["common/wallet/loggedIn"];
+			return loggedIn;
+		},
+	},
 
-
-  },
-
-  created: function () {
-    // Get all the accounts
-    this.$store
-      .dispatch('zigbeealliance.distributedcomplianceledger.pki/QueryChildCertificates', {
-        options: {
-          subscribe: false,
-          all: true
-        },
-        params: {
-          issuer: this.subject,
-          authorityKeyId: this.subjectKeyId,
-        }
-      }).then(
-				(response) => {
-				},
+	created: function () {
+		// Get all the child certificates
+		this.$store
+			.dispatch(
+				"zigbeealliance.distributedcomplianceledger.pki/QueryChildCertificates",
+				{
+					options: {
+						subscribe: false,
+						all: true,
+					},
+					params: {
+						issuer: this.subject,
+						authorityKeyId: this.subjectKeyId,
+					},
+				}
+			)
+			.then(
+				(response) => { },
 				(error) => {
-					this.error = error.message
+					this.error = error.message;
 				}
 			);
-  }
-}
+	},
+};
 </script>
 
 <template>
-  <div class="prime-vue-container ml-5">
-
-		<div v-if="allCertificates.length>0">
-			<DataTable v-model:expandedRows="expandedRows" :value="allCertificates" :auto-layout="true" showGridlines stripedRows>
+	<div class="prime-vue-container">
+		<div v-if="allChildCertificates.length > 0">
+			<DataTable v-model:expandedRows="expandedRows" :value="allChildCertificates" :auto-layout="true" showGridlines
+				stripedRows>
 				<Column :expander="true" headerStyle="width: 3rem" />
-				<Column field="subject" header="Subject"></Column>
+				<Column field="subjectAsText" header="Subject"></Column>
 				<Column field="subjectKeyId" header="Subject Key Id"></Column>
 				<Column headerStyle="width: 4rem; text-align: center" bodyStyle="text-align: center; overflow: visible">
-            <template #body="{ data }">
-              <Button
-                label="Revoke"
-                class="p-button-danger"
-                @click="showRevokeCertificateDialog(data)"
-                iconPos="left"
-                icon="pi pi-ban"
-                v-bind:class="{ 'p-disabled': !isSignedIn }"
-              />
-            </template>
-				</Column>
-					<template #expansion="certificate">
+					<template #body="{ data }">
+						<Button label="Revoke" class="p-button-danger" @click="showRevokeCertificateDialog(data)" iconPos="left"
+							icon="pi pi-ban" v-bind:class="{ 'p-disabled': !isSignedIn }"></Button>
 
-						<div>
-
-							<LeafCertificates :subject="certificate.data.subject" :subjectKeyId="certificate.data.subjectKeyId"></LeafCertificates>
-						</div>
+						<Button label="Download" class="p-button-success mt-1" icon="pi pi-download"
+							@click="downloadCertificateChain(data)"></Button>
 					</template>
-
+				</Column>
+				<template #expansion="certificate">
+					<div>
+						<LeafCertificates :subject="certificate.data.subject" :subjectKeyId="certificate.data.subjectKeyId">
+						</LeafCertificates>
+					</div>
+				</template>
 			</DataTable>
 
-    <Dialog
-      header="Revoke X509 Certificate"
-      @update:visible="dismissRevokeCertificateDialog"
-      :visible="showRevokeCert"
-      :style="{ width: '50vw' }"
-      class="p-fluid"
-      :modal="true">
-      <GrantActionRootCertificate action="RevokeX509Cert" :certificate="revokeCertificate" @close-dialog="dismissRevokeCertificateDialog"></GrantActionRootCertificate>
-    </Dialog>
-
+			<Dialog header="Revoke X509 Certificate" @update:visible="dismissRevokeCertificateDialog"
+				:visible="showRevokeCert" :style="{ width: '50vw' }" class="p-fluid" :modal="true">
+				<GrantActionRootCertificate action="RevokeX509Cert" :certificate="revokeCertificate"
+					@close-dialog="dismissRevokeCertificateDialog"></GrantActionRootCertificate>
+			</Dialog>
 		</div>
 		<div v-else>
-			<h5> No Child Certificates present on ledger </h5>
+			<h5>No Child Certificates present on ledger</h5>
 		</div>
-
-  </div>
+	</div>
 </template>
---
+++ src/views/Validators/GrantActionValidator.vue
@@ -0,0 +1,152 @@
+<script>
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import { required } from '@vuelidate/validators'
+import { useVuelidate } from '@vuelidate/core'
+import Message from 'primevue/message'
+
+export default {
+  name: 'GrantActionValidator',
+
+  components: {
+    InputText,
+    Button,
+    Message
+  },
+
+  props: ['validator', 'action'],
+  setup: () => ({ v$: useVuelidate() }),
+  data() {
+    return {
+      txProcessing: false,
+      error: null
+    }
+  },
+
+  computed: {
+    isSignedIn() {
+      const loggedIn = this.$store.getters['common/wallet/loggedIn']
+      return loggedIn
+    }
+  },
+  methods: {
+    submitApproval() {
+      this.error = null
+      this.txProcessing = true;
+			let loader = this.$loading.show();
+
+
+      if (!this.$store.getters['common/wallet/loggedIn']) {
+        this.$toast.add({ severity: 'warn', summary: 'Please sign in', detail: 'Please sign in to at least one account', life: 3000 })
+        return
+      }
+      const wallet = this.$store.getters['common/wallet/wallet']
+      const accounts = wallet.accounts
+      const account = wallet.accounts[0]
+      const creatorAddress = account.address
+
+      this.$store
+        .dispatch('zigbeealliance.distributedcomplianceledger.validator/sendMsg' + this.action, {
+          value: {
+            creator: creatorAddress,
+            address: this.validator.owner || this.validator.address,
+            info: this.info,
+            time: parseInt(new Date().getTime() / 1000)
+          }
+        })
+        .then(
+          (response) => {
+            this.txProcessing = false;
+						loader.hide();
+            if (response.code != 0) {
+              this.error = response
+            } else {
+              this.error = null
+              this.$toast.add({ severity: 'success', summary: 'Successful Tx', detail: this.action + ' transaction was submitted successfully', life: 3000 })
+              this.$emit('close-dialog')
+            }
+          },
+          (error) => {
+            this.error = error;
+            this.txProcessing = false;
+						loader.hide();
+						this.$toast.add({ severity: 'error', summary: 'Error while processing Tx', detail: 'Tx failed', life: 3000 })
+          }
+        )
+    },
+
+    onClose(e) {
+      e.preventDefault()
+      this.$emit('close-dialog')
+    },
+    errorMessage() {
+      if (this.error && this.error.rawLog) {
+        return this.error.rawLog
+      } else if (this.error) {
+        return this.error
+      } else {
+        return null
+      }
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="grid">
+    <div class="col-12">
+      <Message :closable="false" v-if="error" severity="error">{{ errorMessage() }}</Message>
+
+      <form @submit.prevent="submitApproval">
+        <div class="p-fluid">
+          <div class="p-grid mb-4">
+            <div class="field">
+              <label for="address">Address</label>
+              <InputText id="address" type="text" disabled="true" :value="validator.address" />
+            </div>
+          </div>
+          <div class="field">
+            <label for="info">Optional Information</label>
+            <InputText id="info" type="text" v-model="info" />
+          </div>
+
+          <div class="field">
+            <div class="grid">
+              <div class="col-3">
+                <Button
+                  class="mr-2 mb-2 p-button p-button-primary"
+                  v-if="!txProcessing && action == 'ProposeDisableValidator'"
+                  type="submit"
+                  label="Propose Disable"
+                  icon="pi pi-user"
+                  iconPos="left"
+                />
+                <Button
+                  class="mr-2 mb-2 p-button p-button-primary"
+                  v-if="!txProcessing && action == 'ApproveDisableValidator'"
+                  type="submit"
+                  label="Approve Disable"
+                  icon="pi pi-ban"
+                  iconPos="left"
+                />
+                <Button
+                  class="mr-2 mb-2 p-button p-button-primary"
+                  v-if="txProcessing"
+                  label="Submitted Tx.."
+                  disabled="disabled"
+                  icon="pi pi-spin pi-spinner"
+                  iconPos="left"
+                />
+              </div>
+              <div class="col-3">
+                <Button class="mr-2 mb-2 p-button p-button-secondary" v-on:click="onClose" label="Cancel" icon="pi pi-times" iconPos="left" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
