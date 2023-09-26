5837a3a3d8d4d46e02fcc75db9cc0429f1cf4917
Jain, Ankur <Ankur_Jain@comcast.com>
2023-09-26 11:01:45 -0400

    Toggle Switch to Select the wallet

--- src/AppWallet.vue
+++ src/AppWallet.vue
@@ -1,13 +1,80 @@
 <template>
-	<SpWallet ref="wallet" v-on:dropdown-opened="$refs.menu.closeDropdown()" />
+  <div class="main-container">
+    <div class="wallet-container">
+      <KeplrWallet v-if="isKeplrWallet" ref="wallet" />
+      <SpWallet v-else ref="wallet" />
+    </div>
+    <div ref="switchContainer" class="switch-container">
+      <InputSwitch v-model="isKeplrWallet" @change="toggleWallet" v-tooltip="'Toggle between SPWallet and Keplr Wallet'" />
+    </div>
+   
+  </div>
 </template>
 
 <script>
+import { ref, onMounted, computed } from 'vue';
+import KeplrWallet from "@/components/KeplrWallet.vue";
+import SpWallet from "@/components/SpWallet.vue";
+import InputSwitch from 'primevue/inputswitch';
+import Tooltip from 'primevue/tooltip';
+
 export default {
-
+  components: {
+    KeplrWallet,
+    SpWallet,
+    InputSwitch,
+    Tooltip,
+  },
+  setup() {
+    const isKeplrWallet = ref(true); // Default is KeplrWallet, so the switch is "on"
+    const switchContainer = ref(null);
+    
+    const tooltipContent = computed(() => {
+      return isKeplrWallet.value ? 'Using New Wallet (KeplrWallet)' : 'Using Old Wallet (SpWallet)';
+    });
+    
+    const target = computed(() => {
+      return switchContainer.value ? ".switch-container" : "";
+    });
+    
+    const toggleWallet = () => {
+      // Assuming that there is a 'toggle' method in both wallet components, you might need to adjust this as per your implementation
+      if (this.$refs.wallet && this.$refs.wallet.toggle) this.$refs.wallet.toggle();
+    }
+    
+    // onMounted(() => {
+    //   if (!switchContainer.value) {
+    //     console.warn('Switch Container not found. Tooltip will not be attached.');
+    //   }
+    // });
+    
+    return { isKeplrWallet, toggleWallet, tooltipContent, switchContainer, target };
+  },
 }
-
 </script>
 
+<style scoped>
+.main-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 
+.switch-container {
+  position: relative;
+  margin-right: 10px; /* Adjust as needed */
+	margin-left: 10px; /* Adjust as needed */
+}
 
+.wallet-container {
+  flex-grow: 1;
+}
+
+.sp-wallet {
+    position: relative;
+    z-index: 90;
+		right: 0%;
+    transform: scale(0.7) translateX(-50%) !important;
+		left: 50%;
+}
+</style>
---
+++ src/components/KeplrWallet.vue
@@ -0,0 +1,208 @@
+
+<template>
+  <div>
+    <div v-if="!selectedKeplrAccount">
+      <Button
+        @click="connect"
+        class="wallet-button"
+        label="Connect Keplr"
+      ></Button>
+    </div>
+    <div v-else>
+      <SplitButton
+        class="keplr-split-button"
+        :label="`${accountName} (${trimAddress(selectedKeplrAccount.address)})`"
+        :model="menuItems"
+      ></SplitButton>
+    </div>
+
+    <Dialog
+      header="Keplr Wallet"
+      :visible="dialogVisible"
+      :modal="true"
+      :closable="true"
+      :width="400"
+    >
+      <p>
+        To use this application, you need to have the Keplr wallet extension
+        installed in your browser.
+      </p>
+      <p>
+        If you're using Google Chrome, you can install it from the
+        <a
+          href="https://chrome.google.com/webstore/detail/keplr/dmkamcknogkgcdfhhbddcghachkejeap"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Chrome Web Store</a
+        >.
+      </p>
+      <p>
+        If you're using Mozilla Firefox, you can install it from the
+        <a
+          href="https://addons.mozilla.org/en-US/firefox/addon/keplr/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Firefox Add-ons Store</a
+        >.
+      </p>
+
+      <Button
+        label="Close"
+        class="p-mt-2"
+        @click="dialogVisible = false"
+      ></Button>
+    </Dialog>
+  </div>
+</template>
+
+<script>
+import Button from "primevue/button";
+import Dialog from "primevue/dialog";
+import SplitButton from "primevue/splitbutton";
+import { mapState } from "vuex";
+
+export default {
+  components: {
+    Button,
+    Dialog,
+    SplitButton,
+  },
+  computed: {
+    ...mapState(["selectedKeplrAccount"]),
+  },
+  data() {
+    return {
+      dialogVisible: false,
+      accountName: null,
+      menuItems: [
+        {
+          label: "Disconnect Keplr",
+          command: () => {
+            this.disconnect();
+          },
+        },
+      ],
+    };
+  },
+  methods: {
+    trimAddress(address) {
+      // Return first 6 and last 4 characters
+      if (address && address.length > 16) {
+        return (
+          address.substring(0, 10) +
+          "..." +
+          address.substring(address.length - 4)
+        );
+      } else {
+        return address;
+      }
+    },
+
+    async connect() {
+      const keplr = window.keplr;
+      if (!keplr) {
+        this.dialogVisible = true;
+        return;
+      }
+      const chainId = process.env.VUE_APP_DCL_CHAIN_ID
+      const chainInfo = {
+        // Update this with actual values
+        chainId,
+        chainName: "DCL Chain",
+        rpc: process.env.VUE_APP_DCL_RPC_NODE,
+        rest: process.env.VUE_APP_DCL_API_NODE,
+        stakeCurrency: {
+          coinDenom: "DCL",
+          coinMinimalDenom: "DCL",
+          coinDecimals: 6,
+        },
+        bip44: {
+          coinType: 118,
+        },
+        bech32Config: {
+          bech32PrefixAccAddr: "cosmos",
+          bech32PrefixAccPub: "cosmospub",
+          bech32PrefixValAddr: "cosmosvaloper",
+          bech32PrefixValPub: "cosmosvaloperpub",
+          bech32PrefixConsAddr: "cosmosvalcons",
+          bech32PrefixConsPub: "cosmosvalconspub",
+        },
+        currencies: [
+          {
+            coinDenom: "DCL",
+            coinMinimalDenom: "DCL",
+            coinDecimals: 6,
+          },
+        ],
+        feeCurrencies: [
+          {
+            coinDenom: "DCL",
+            coinMinimalDenom: "DCL",
+            coinDecimals: 6,
+          },
+        ],
+        gasPriceStep: {
+          low: 0.0,
+          average: 0.0,
+          high: 0.0,
+        },
+      };
+
+      try {
+        await keplr.experimentalSuggestChain(chainInfo);
+      } catch (error) {
+        // This error might be thrown if the chain already exists, but you should inspect the error message to determine this
+        if (error.message !== "Chain already exists.") {
+          this.$toast.add({
+                  severity: 'error',
+                  summary: 'Error Adding the DCL Chain to Keplr Wallet',
+                  detail: error.message,
+                  life: 5000
+              });
+              return;
+        }
+      }
+
+      try {
+        await keplr.enable(chainId);
+      } catch (error) {
+        this.$toast.add({
+                  severity: 'error',
+                  summary: 'Error Enabling the DCL Chain on the Keplr Wallet',
+                  detail: error.message,
+                  life: 5000
+              });
+      }
+
+      try {
+        await this.$store.dispatch("setKeplrSigner");
+        const key = await keplr.getKey(process.env.VUE_APP_DCL_CHAIN_ID);
+        this.accountName = key.name;
+        this.$toast.add({
+          severity: "success",
+          summary: "Connected to Keplr wallet",
+          life: 5000,
+        });        
+      } catch (error) {
+        console.error("Error fetching account name:", error);
+      }
+    },
+
+    disconnect() {
+      this.$store.dispatch("disconnectKeplr");
+      console.log("Disconnected from Keplr wallet");
+    },
+    
+    closeDialog() {
+      this.dialogVisible = false;
+    },
+    // ... rest of the methods, created, etc.
+  },
+};
+</script>
+
+<style scoped>
+.keplr-split-button {
+  /* Style the keplr split button as needed */
+}
+</style>
