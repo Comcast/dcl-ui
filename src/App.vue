7a38b30f57382549b3bb3e870a14ab98ed745f24
Jain, Ankur <Ankur_Jain@comcast.com>
2024-07-26 12:57:38 -0400

    Add documentation

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,8 @@ export default {
                         { label: 'PKI', icon: 'pi pi-fw pi-lock', to: '/pki' },
                         { label: 'Validators', icon: 'pi pi-fw pi-server', to: '/validators' },
                         { label: 'Upgrades', icon: 'pi pi-fw pi-history', to: '/upgrades' },
-                        { label: 'Legacy Wallet', icon: 'pi pi-fw pi-wallet', to: '/legacy-wallet' }
+                        { label: 'Legacy Wallet', icon: 'pi pi-fw pi-wallet', to: '/legacy-wallet' },
+                        { label: 'Keplr Wallet - Guide', icon: 'pi pi-fw pi-book', to: '/keplr-wallet' }
 
                     ]
                 }
